1f3de7a18aeef3c23d14b7e9b677f0850d0ae1a4
Ravi Sharma <ravis4376@gmail.com>
2017-01-31 00:56:38 +0530

    added tests

--- test/com/thoughtworks/assignment1/shared_memory_test.rb
+++ test/com/thoughtworks/assignment1/shared_memory_test.rb
@@ -1,7 +1,8 @@
+require_relative '../../../../src/com/thoughtworks/assignment1/config'
 require 'test/unit'
 require 'test/unit/ui/console/testrunner'
 
-class TC_MyTest < Test::Unit::TestCase
+class SharedMemoryTest < Test::Unit::TestCase
 
   def self.startup
     require_relative '../../../../src/com/thoughtworks/assignment1/start'
@@ -11,13 +12,16 @@ class TC_MyTest < Test::Unit::TestCase
     system("kill -9 $(lsof -Pti @localhost:#{CONFIG['first_process_port']}-#{CONFIG['first_process_port']+CONFIG['process_count']} -sTCP:LISTEN)")
   end
 
-  def test_should_data
-    port = 3000
-    socket = TCPSocket.new(CONFIG['host'], port)
-    socket.write 'GET 0'
-    assert_equal(0,socket.recv(1024).to_i)
+  CONFIG['raw_data'].length.times do |address|
+    (CONFIG['first_process_port']..(CONFIG['first_process_port']+CONFIG['process_count']-1)).to_a.each do |port|
+      define_method("test_should_get_data_on_address_#{address}_through_process_port_#{port}") do
+        socket = TCPSocket.new(CONFIG['host'], port)
+        socket.write "GET #{address}"
+        assert_equal(address, socket.recv(1024).to_i)
+      end
+    end
   end
-
 end
+
 
 Test::Unit::UI::Console::TestRunner.run(TC_MyTest)
