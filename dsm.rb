9044462e1dae103c2a8d135cd4e8af47465a47f4
Ravi Sharma <ravis4376@gmail.com>
2017-01-30 15:20:20 +0530

    working solution

--- dsm.rb
+++ dsm.rb
@@ -9,35 +9,40 @@ class Dsm
 
   def write(address, new_value)
     if locally_present?(address)
+      puts "Address: #{address} is locally present"
       @index_map.each do |key, value|
         if value == address
           @data[key] = new_value
         end
       end
     else
-      process_number = AddressMapper.new.get_process_for(address)
-      puts "Forwarding request to process running on port #{CONFIG['first_process_port']+process_number}"
-      sock = TCPSocket.new(CONFIG['host'], CONFIG['first_process_port']+process_number)
-      sock.write "PUT #{address} #{new_value}\000"
+      forward_request(address, "PUT #{address} #{new_value}\000")
     end
   end
 
   def read(address)
     if locally_present?(address)
+      puts "Address: #{address} is locally present"
       @index_map.each do |key, value|
         return @data[key] if value == address
       end
     else
-      process_number = AddressMapper.new.get_process_for(address)
-      puts "Forwarding request to process running on port #{CONFIG['first_process_port']+process_number}"
-      sock = TCPSocket.new(CONFIG['host'], CONFIG['first_process_port']+process_number)
-      sock.write "GET #{address}\000"
-      sock.recv(1024)
+      socket = forward_request(address, "GET #{address}\000")
+      socket.recv(1024)
     end
   end
 
   private
   def locally_present?(address)
     @index_map.values.include?(address)
+  end
+
+  def forward_request(address, request)
+    process_number = AddressMapper.new.get_process_for(address)
+    puts "Address: #{address} is present at server running on port #{CONFIG['first_process_port']+process_number}"
+    puts "Forwarding request to server running on port #{CONFIG['first_process_port']+process_number}"
+    socket = TCPSocket.new(CONFIG['host'], CONFIG['first_process_port']+process_number)
+    socket.write request
+    socket
   end
 end
--- server.rb
+++ server.rb
@@ -12,27 +12,45 @@ class Server
   def accept_requests
     loop {
       connection = @tcp_server.accept
-      puts("Request received on server running on port #{@port}")
+      puts("Request received at server running on port #{@port}")
       request = connection.recv(1024)
-      if request =~ /(PUT\s\d\s\d|GET\s\d)/
-        request_params = request.split
-        if request_params[0] == 'GET'
-          connection.write("#{@dsm.read(request_params[1].to_i)}\n")
-        else
-          @dsm.write(request_params[1].to_i, request_params[2].to_i)
-        end
-      else
-        connection.write('Invalid Request')
-      end
+      connection.write("#{process_request(request.split)}\n")
       connection.close
     }
   end
 
   private
-  def forward_request_to(port, request)
-    socket = TCPSocket.new(CONFIG['host'], port) # could replace 127.0.0.1 with your "real" IP if desired.
-    socket.write(request)
-    puts "got back: #{socket.recv(1024)}"
-    socket.close
+  def valid_address?(address)
+    last_index >= address
+  end
+
+  def process_request(request_params)
+    method = request_params[0]
+    address = request_params[1].to_i
+    if method =~ /(PUT|GET)/ and valid_address?(address)
+      if method == 'GET'
+        process_get(address)
+      else
+        process_put(address, request_params[2].to_i)
+      end
+    else
+      "Invalid Request: Only GET and PUT are accepted and address should be between 0 and #{last_index}\n"
+    end
+  end
+
+
+  def process_get(address)
+    puts("GET request to read value at address #{address}")
+    @dsm.read(address)
+  end
+
+  def process_put(address, new_value)
+    puts("PUT request to update value of address #{address} to #{new_value}")
+    @dsm.write(address, new_value)
+    "Successfully updated the value of address #{address} with #{new_value}"
+  end
+
+  def last_index
+    CONFIG['raw_data'].length - 1
   end
 end
